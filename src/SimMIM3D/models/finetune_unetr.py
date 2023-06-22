16694d28a8105b7dd75a629c2c0856a77927e516
Tim Riedel <timriedel00@gmail.com>
2023-06-22 10:03:51 +0200

    refactor: disable learning rate scheduler

--- src/SimMIM3D/models/finetune_unetr.py
+++ src/SimMIM3D/models/finetune_unetr.py
@@ -48,12 +48,14 @@ class FinetuneUNETR(pl.LightningModule):
             weight_decay=self.weight_decay
         )
 
-        lr_scheduler = WarmupCosineSchedule(
-            optimizer=optimizer,
-            warmup_steps=self.warmup_epochs,
-            t_total=self.epochs + self.warmup_epochs
-        )
-        return {"optimizer": optimizer, "lr_scheduler": {"scheduler": lr_scheduler, "interval": "epoch"}}
+        return {"optimizer": optimizer}
+        # TODO: reenable warmup scheduler
+        # lr_scheduler = WarmupCosineSchedule(
+        #     optimizer=optimizer,
+        #     warmup_steps=self.warmup_epochs,
+        #     t_total=self.epochs + self.warmup_epochs
+        # )
+        # return {"optimizer": optimizer, "lr_scheduler": {"scheduler": lr_scheduler, "interval": "epoch"}}
 
     def prepare_batch(self, batch):
         return batch["image"], batch["label"]
--- src/SimMIM3D/models/pretrain_simmim.py
+++ src/SimMIM3D/models/pretrain_simmim.py
@@ -33,12 +33,13 @@ class PretrainSimMIM(pl.LightningModule):
             weight_decay=self.weight_decay
         )
 
-        lr_scheduler = WarmupCosineSchedule(
-            optimizer=optimizer,
-            warmup_steps=self.warmup_epochs,
-            t_total=self.epochs + self.warmup_epochs
-        )
-        return {"optimizer": optimizer, "lr_scheduler": {"scheduler": lr_scheduler, "interval": "epoch"}}
+        return {"optimizer": optimizer}
+        # lr_scheduler = WarmupCosineSchedule(
+        #     optimizer=optimizer,
+        #     warmup_steps=self.warmup_epochs,
+        #     t_total=self.epochs + self.warmup_epochs
+        # )
+        # return {"optimizer": optimizer, "lr_scheduler": {"scheduler": lr_scheduler, "interval": "epoch"}}
 
     def prepare_batch(self, batch):
         return batch["image"], batch["mask"]
